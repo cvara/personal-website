3709cc427092e8b9449677c06b5d2c764554c45c
Christoforos Varakliotis <c.varakliotis@gmail.com>
2024-10-16 19:42:32 +0300

    add color palette to tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   content: [],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      teal: "#00BCD4",
+      purlple: "#8E44AD",
+      navy: "#1C2833",
+      gray: {
+        light: "#F5F7FA",
+        DEFAULT: "#BDC3C7",
+        dark: "#889096",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [`gatsby-plugin-postcss`],
 };
