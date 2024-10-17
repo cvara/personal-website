062239cc216d256ff7ff322b5c62ea22ea2d5af8
Christoforos Varakliotis <c.varakliotis@gmail.com>
2024-10-17 09:51:51 +0300

    replace gatsby info with mine

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const tailwindConfig = require("./tailwind.config.js");
+
 /**
  * Configure your Gatsby site with this file.
  *
@@ -9,10 +11,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Christoforos - Software Engineer & Web Developer`,
+    description: `Welcome to the personal website of Christoforos Varakliotis, a software engineer specializing in web development, projects, and insights`,
+    author: `Christoforos Varakliotis`,
+    siteUrl: `https://christoforos.me`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -28,15 +30,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `christoforos-varakliotis-software-engineer`,
+        short_name: `christoforos`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: tailwindConfig.theme.colors.purple,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        // theme_color: tailwindConfig.theme.colors.purple,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
   ],
