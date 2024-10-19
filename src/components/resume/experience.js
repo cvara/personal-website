2d746ed1d69968a0add5e6e8d08fde5493399755
Christoforos Varakliotis <c.varakliotis@gmail.com>
2024-10-19 19:16:24 +0300

    resume updates

--- src/components/resume/experience.js
+++ src/components/resume/experience.js
@@ -25,38 +25,42 @@ const Experience = ({ experience, className }) => (
       "group [&:not(:last-child)]:pb-10 relative pl-4 ml-2",
     )}
   >
-    <div className="w-[2px] group-last:w-0 absolute left-0 top-2 -bottom-2 bg-purple">
-      <div className="w-[12px] h-[12px] rounded-full bg-purple absolute top-[-6px] left-[-5px]" />
+    <div className="w-[2px] group-last:w-0 absolute left-0 top-2 -bottom-2 bg-gradient-to-b from-purple to-teal">
+      <div className="w-[12px] h-[12px] rounded-full bg-gradient-to-tr from-purple to-teal absolute top-[-6px] left-[-5px]" />
     </div>
-    <div className="text-xs text-purple font-bold mb-2">
+    <div className="text-xs bg-gradient-to-tr from-purple to-teal bg-clip-text text-transparent font-bold mb-2">
       {experience.start} - {experience.end}
     </div>
 
-    <div className="flex gap-2 items-center mb-2">
+    <div className="flex gap-2 items-center mb-2 max-md:flex-col max-md:items-start max-md:gap-1">
       <div className="text-lg font-bold">{experience.title}</div>
-      &middot;
-      <div className="text-md">
-        {experience.companyUrl ? (
-          <a
-            href={experience.companyUrl}
-            target="_blank"
-            rel="noreferrer"
-            className="bg-gradient-to-tr from-purple to-teal text-transparent bg-clip-text font-bold border-b-2 border-purple hover:text-purple"
-          >
-            {experience.company}
-          </a>
-        ) : (
-          <div className="bg-gradient-to-tr from-purple to-teal text-transparent bg-clip-text font-bold">
-            {experience.company}
-          </div>
+      <span className="max-md:hidden">&middot;</span>
+      <div className="flex gap-2 items center max-md:mb-2">
+        <div className="text-md">
+          {experience.companyUrl ? (
+            <a
+              href={experience.companyUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="bg-gradient-to-tr from-purple to-teal text-transparent bg-clip-text font-bold border-b-2 border-purple hover:text-purple"
+            >
+              {experience.company}
+            </a>
+          ) : (
+            <div className="bg-gradient-to-tr from-purple to-teal text-transparent bg-clip-text font-bold">
+              {experience.company}
+            </div>
+          )}
+        </div>
+        {experience.occupation && (
+          <>
+            &middot;
+            <div className="text-sm text-gray-dark">
+              {experience.occupation}
+            </div>
+          </>
         )}
       </div>
-      {experience.occupation && (
-        <>
-          &middot;
-          <div className="text-sm text-gray-dark">{experience.occupation}</div>
-        </>
-      )}
     </div>
 
     <div
