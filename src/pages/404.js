97a7a2e8d9d541bcb5a280c094c308af2c96b773
Christoforos Varakliotis <c.varakliotis@gmail.com>
2024-10-25 15:20:36 +0300

    restyle 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,15 @@ import Seo from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="flex flex-col items-center justify-center gap-4 grow shrink">
+      <h1 className="text-6xl sm:text-8xl font-bold mb-0 bg-gradient-to-tr from-purple to-teal bg-clip-text text-transparent">
+        404
+      </h1>
+      <div className="text-4xl font-extralight">Not Found</div>
+      <div className="text-lg font-extralight text-gray-dark text-center">
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </div>
+    </div>
   </Layout>
 );
 
